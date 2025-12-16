ea735f0f82a853071c22095da52227b3eb5d256e
Pwsawbty <basumatarypwsaw@gmail.com>
2025-12-16 19:59:10 +0530

    git add src/core/signals.ts src/core/history.ts src/core/postInput.ts src/index.ts git commit -m "feat(core): add minimal post decision input contract (signals v1)

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ export * from "./core/intents";
 export * from "./core/platforms";
 export * from "./core/boundaries";
 export * from "./core/version";
+
+export * from "./core/signals";
+export * from "./core/history";
+export * from "./core/postInput";
